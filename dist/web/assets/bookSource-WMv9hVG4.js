1c3be4c9ad3fb309948f05f4cd25516ebefb78dc
gfyuye <gfyuye@hotmail.com>
2026-09-15 14:20:02 +0000

    移除 dist 目录，添加 .gitignore

--- dist/web/assets/bookSource-WMv9hVG4.js
+++
@@ -1,2 +0,0 @@
-import{g as s,p as r}from"./http-CFi3ScYz.js";async function c(o){return s("/getBookSources",{userNameSpace:o})}async function n(o,e){return r("/saveBookSource",{bookSource:o,userNameSpace:e})}async function u(o,e){return r("/deleteBookSource",{sourceUrl:o,userNameSpace:e})}export{u as d,c as g,n as s};
-//# sourceMappingURL=bookSource-WMv9hVG4.js.map
